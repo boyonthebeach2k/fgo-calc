9aeb30807094419089b459c1f22af6b824c5b40c
boyonthebeach2k <39589879+boyonthebeach2k@users.noreply.github.com>
2022-05-06 00:59:50 +0530

    Fix bug due to operator precedence

--- src/commands/calc.ts
+++ src/commands/calc.ts
@@ -150,7 +150,7 @@ const commandObjectToCalcTerms = (svt: Servant.Servant | Enemy.Enemy, args: Part
 
     let triangleModifier = f32(args.classOverride ?? (classRelation[svt.className]?.[enemyClass] ?? 1000) / f32(1000));
 
-    let attributeModifier = f32(attributeRelation[svt.attribute]?.[enemyAttribute] ?? 1000 / f32(1000));
+    let attributeModifier = f32((attributeRelation[svt.attribute]?.[enemyAttribute] ?? 1000) / f32(1000));
 
     let extraCardModifier: 1 | 2 | 3.5 = args.extra ? 2 : 1;
 
